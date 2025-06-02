472246e61ccd09d7439b7f9c18eba4a1746d45c1
ASUS <jc990029@mail-ljcu.jcu.edu.au>
2025-06-02 11:26:46 +0800

    after change

--- prac_02/score.py
+++ prac_02/score.py
@@ -5,13 +5,29 @@ Broken program to determine score status
 
 # : Fix this!
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
+import random
+from random import randint
+
+
+def main():
+
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    print(define_result(score))
+
+    random_score = random.randint(1,100)
+    print(random_score)
+    print(define_result(random_score))
+
+def define_result(score):
     if score < 50:
-        print("Bad")
+        score_result = "Bad"
     elif score < 90:
-        print("Passable")
+        score_result = "Passable"
     elif score >= 90:
-        print("Excellent")
+        score_result = "Excellent"
+    return score_result
+
+main()
