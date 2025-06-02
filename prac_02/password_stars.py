dc6fd881cc31264f86d8047a5efe496dce7a4e2a
ASUS <jc990029@mail-ljcu.jcu.edu.au>
2025-06-02 10:06:41 +0800

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,18 @@
 def main():
     length = 5
+    password = get_password(length)
+    password_stars(password)
+
+def get_password(length):
     password = input("What is your password: ")
     while len(password) < length:
         print("Password is not long enough.")
         password = input("What is your password: ")
-    password_stars(password)
+    return password
+
 
 def password_stars(password):
-    print(len(password) * "*")
+    print_style = "*"
+    print(len(password) * print_style)
 
 main()
