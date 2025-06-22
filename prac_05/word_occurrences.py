17e79542bd00bbec697e6205d1c53ee2b1e860e3
ASUS <jc990029@mail-ljcu.jcu.edu.au>
2025-06-22 20:56:47 +0800

    count words

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,3 +6,9 @@ Actual:    minutes
 
 word_counts = {}
 words_string = input("Text: ").lower()
+words = words_string.split()
+for word in words:
+    try:
+        word_counts[word] += 1
+    except KeyError:
+        word_counts[word] = 1
