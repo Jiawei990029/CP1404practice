a359ce1606ad70a415ae598a95b34a21e29bf617
ASUS <jc990029@mail-ljcu.jcu.edu.au>
2025-06-23 19:57:40 +0800

    Calculate how many times each champion has won

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,3 +3,19 @@ wimbledon
 Estimate: 10 minutes
 Actual:    minutes
 """
+
+def main():
+    with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
+        all_lists = [line.strip().split(',') for line in in_file]
+        all_lists.remove(all_lists[0])
+    champion_to_country = {}
+    for line_list in all_lists:
+        try:
+            champion_to_country[line_list[2]] += 1
+        except KeyError:
+            champion_to_country[line_list[2]] = 1
+    print(champion_to_country)
+
+
+
+main()
