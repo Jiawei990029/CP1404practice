34ba99b83ae6047ac6e493adb181d12a1aec2bfb
ASUS <jc990029@mail-ljcu.jcu.edu.au>
2025-07-06 20:00:16 +0800

    output guitars information

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,7 +1,7 @@
 """
 guitars
 Estimate: 10 minutes
-Actual:   20 minutes
+Actual:   45 minutes
 """
 
 from prac_06.guitar import Guitar
@@ -15,8 +15,14 @@ def main():
         cost = float(input("Cost: $"))
         guitars.append(Guitar(name, year, cost))
         print(f"{name} ({year}) : ${cost} added.")
-    print("")
+        name = input("Name: ")
 
-
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage(guitar.get_age(guitar.year)):
+            vintage_string = "(vintage)"
+        else:
+            vintage_string = ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 main()
