44a305e1227c3510714136d19da1aa81ceb1b0d4
ASUS <jc990029@mail-ljcu.jcu.edu.au>
2025-07-31 23:35:47 +0800

    finish

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -15,7 +15,31 @@ def main():
     while choice != "q":
         if choice == "c":
             current_taxi = choose_taxi(taxis)
+        elif choice == "d":
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                cost = drive_taxi(current_taxi)
+                total_bill += cost
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${total_bill:.2f}")
+        print(MENU)
+        choice = input(">>> ").lower()
 
+    print(f"Total trip cost: ${total_bill:.2f}")
+    print("Taxis are now:")
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
+
+def drive_taxi(taxi):
+    distance = float(input("Drive how far? "))
+    distance_driven = taxi.drive(distance)
+    cost = taxi.get_fare()
+    taxi.start_fare()  # Reset the fare for next trip
+    print(f"Your {taxi.name} trip cost you ${cost:.2f}")
+    return cost
 
 def choose_taxi(taxis):
     print("Taxis available: ")
@@ -30,6 +54,5 @@ def choose_taxi(taxis):
     except ValueError:
         print("Invalid taxi choice")
     return None
-
 
 main()
