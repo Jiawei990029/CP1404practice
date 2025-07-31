de35df984560eca7140774b391e36ccb1c31ab1c
ASUS <jc990029@mail-ljcu.jcu.edu.au>
2025-07-31 23:14:23 +0800

    Create menu

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,17 @@
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    total_bill = 0.0
+
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
+
+
+
+main()
